60887f12f978f9999f3ec9f66da85aa7fcd9fc3f
2333xxxx <ctc20120114@163.com>
2025-08-30 14:19:39 +0800

    Replaced real API requests with hardcoded mock data (only fetchCustomBackendError doesn't change) Hardcoded values include: token: 'token-test' refreshToken: 'refreshToken-test' userId: 'userId-test' userName: 'userName-test' roles: ['roles-test'] buttons: ['buttons-test'] route: id: '1', name: 'test', path: '/test', component: 'component-test', children: [] home: '404' route existence: true

--- frontend/src/service/api/auth.ts
+++ frontend/src/service/api/auth.ts
@@ -1,4 +1,4 @@
-import { request } from '../request';
+import { request } from "../request";
 
 /**
  * Login
@@ -7,19 +7,35 @@ import { request } from '../request';
  * @param password Password
  */
 export function fetchLogin(userName: string, password: string) {
-  return request<Api.Auth.LoginToken>({
-    url: '/auth/login',
-    method: 'post',
+  // return request<Api.Auth.LoginToken>({
+  //   url: '/auth/login',
+  //   method: 'post',
+  //   data: {
+  //     userName,
+  //     password
+  //   }
+  // });
+  return Promise.resolve({
     data: {
-      userName,
-      password
-    }
+      token: "token-test",
+      refreshToken: "refreshToken-test",
+    },
+    error: false,
   });
 }
 
 /** Get user info */
 export function fetchGetUserInfo() {
-  return request<Api.Auth.UserInfo>({ url: '/auth/getUserInfo' });
+  // return request<Api.Auth.UserInfo>({ url: "/auth/getUserInfo" });
+  return Promise.resolve({
+    data: {
+      userId: "userId-test",
+      userName: "userName-test",
+      roles: ["roles-test"],
+      buttons: ["buttons-test"],
+    },
+    error: false,
+  });
 }
 
 /**
@@ -28,12 +44,19 @@ export function fetchGetUserInfo() {
  * @param refreshToken Refresh token
  */
 export function fetchRefreshToken(refreshToken: string) {
-  return request<Api.Auth.LoginToken>({
-    url: '/auth/refreshToken',
-    method: 'post',
+  // return request<Api.Auth.LoginToken>({
+  //   url: "/auth/refreshToken",
+  //   method: "post",
+  //   data: {
+  //     refreshToken,
+  //   },
+  // });
+  return Promise.resolve({
     data: {
-      refreshToken
-    }
+      token: "token-test",
+      refreshToken: "refreshToken-test",
+    },
+    error: false,
   });
 }
 
@@ -44,5 +67,5 @@ export function fetchRefreshToken(refreshToken: string) {
  * @param msg error message
  */
 export function fetchCustomBackendError(code: string, msg: string) {
-  return request({ url: '/auth/error', params: { code, msg } });
+  return request({ url: "/auth/error", params: { code, msg } });
 }
